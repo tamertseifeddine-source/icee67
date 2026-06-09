f1ed49727a7513b5885bc8101aed8a2562d88cc8
Seif <tamertseifeddine@gmail.com>
2026-06-09 21:38:05 +0100

    First version

---
+++ app.js
@@ -0,0 +1,187 @@
+const colorSelect = document.getElementById("color");
+const statusDiv = document.getElementById("status");
+
+let cart = [];
+
+const cartDiv = document.getElementById("cart");
+
+if (colorSelect) {
+
+  colorSelect.addEventListener("change", () => {
+
+    const color = colorSelect.value;
+
+    if (color === "أخضر") {
+
+      statusDiv.innerHTML = "Non disponible ❌";
+      statusDiv.style.color = "#ff4d4d";
+
+    } else {
+
+      statusDiv.innerHTML = "Disponible ✅";
+      statusDiv.style.color = "#79ffae";
+
+    }
+
+  });
+
+}
+
+function changeMedia(element) {
+
+  const mainImage = document.getElementById("mainImage");
+  const mainVideo = document.getElementById("mainVideo");
+
+  // remove active from all thumbs
+  document.querySelectorAll(".thumb")
+    .forEach(el => el.classList.remove("active"));
+
+  element.classList.add("active");
+
+  // IMAGE
+  if (element.tagName === "IMG") {
+
+    mainVideo.style.display = "none";
+    mainImage.style.display = "block";
+
+    mainImage.src = element.src;
+  }
+
+  // VIDEO
+  else if (element.tagName === "VIDEO") {
+
+    mainImage.style.display = "none";
+    mainVideo.style.display = "block";
+
+    mainVideo.load();
+  }
+}
+
+document
+.getElementById("addToCart")
+.addEventListener("click", () => {
+
+  const product = {
+
+    name: document.body.dataset.product,
+
+    price: document.body.dataset.price,
+
+    color: document.getElementById("color").value
+
+  };
+
+  cart.push(product);
+
+  displayCart();
+
+});
+
+function displayCart() {
+
+  cartDiv.innerHTML = "";
+
+  cart.forEach((item, index) => {
+
+    cartDiv.innerHTML += `
+
+      <div class="cart-item">
+
+        <h4>${item.name}</h4>
+
+        <p>💰 ${item.price}</p>
+
+        <p>🎨 ${item.color}</p>
+
+        <button
+        onclick="removeFromCart(${index})"
+        class="remove-btn">
+
+          Supprimer
+
+        </button>
+
+      </div>
+
+    `;
+
+  });
+
+}
+
+function removeFromCart(index) {
+
+  cart.splice(index, 1);
+
+  displayCart();
+
+}
+
+
+document
+.getElementById("orderForm")
+.addEventListener("submit", async function (e) {
+
+  e.preventDefault();
+
+  const data = {
+
+    firstName:
+    document.getElementById("firstName").value,
+
+    lastName:
+    document.getElementById("lastName").value,
+
+    phone:
+    document.getElementById("phone").value,
+
+    wilaya:
+    document.getElementById("wilaya").value,
+
+    address:
+    document.getElementById("address").value,
+
+    cart: cart.length > 0
+    ? cart
+    : [
+        {
+          name:
+          document.body.dataset.product,
+
+          price:
+          document.body.dataset.price,
+
+          color:
+          document.getElementById("color").value
+        }
+      ]
+
+  };
+
+  try {
+
+    const response = await fetch("http://localhost:3000/order", {
+
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify(data)
+
+    });
+
+    const result = await response.json();
+
+    alert(result.message);
+
+  } catch (error) {
+
+    console.log(error);
+
+    alert("Erreur serveur ❌");
+
+  }
+
+});
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const TOKEN = "8732235422:AAHaw2iSZMDM2MMIXifYkrJW1spzv3YkI10";
+const CHAT_ID = "2144877853";
+
+app.post("/order", async (req, res) => {
+
+  console.log(req.body);
+
+  const {
+    firstName,
+    lastName,
+    phone,
+    wilaya,
+    address,
+    cart
+  } = req.body;
+
+  let productsText = "";
+
+  if (cart && cart.length > 0) {
+
+    cart.forEach((item, index) => {
+
+      productsText += `
+📦 Produit ${index + 1}: ${item.name}
+
+💰 Prix: ${item.price}
+
+🎨 Couleur: ${item.color}
+
+`;
+
+    });
+
+  }
+
+  const message = `
+🛒 Nouvelle commande
+
+${productsText}
+
+👤 Nom: ${firstName}
+
+👤 Prénom: ${lastName}
+
+📞 Téléphone: ${phone}
+
+📍 Wilaya: ${wilaya}
+
+🏠 Adresse: ${address}
+`;
+
+  try {
+
+    await axios.post(
+      `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+      {
+        chat_id: CHAT_ID,
+        text: message
+      }
+    );
+
+    res.json({
+      message: "Commande envoyée ✅"
+    });
+
+  } catch (error) {
+
+    console.log(error.response?.data || error);
+
+    res.status(500).json({
+      message: "Erreur serveur ❌"
+    });
+
+  }
+
+});
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+
+  console.log("Server running on port 3000");
+
+});
